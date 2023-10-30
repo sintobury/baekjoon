8966c87544a598c992594bf8ae437cd4f5169ed7
ChanUk <herkongho@gmail.com>
2023-10-30 13:12:39 +0900

    10.30문제풀이

--- Programers/level2/H-index.js
+++ Programers/level2/H-index.js
@@ -2,11 +2,10 @@ function solution(citations) {
     const n = citations.length;
     const sort = citations.slice().sort((a,b)=>a-b);
     let answer =0;
-    for(let i=0; i<n; i++) {
-        const h = sort[i];
-        const high = sort.filter((el)=> el>=h ).length;
-        if( high >= h && n-high <= h ) {
-            answer = h;
+    for(let i=1; i<n+1; i++) {
+        const high = sort.filter((el)=> el>=i ).length;
+        if( high >= i && n-high <= i ) {
+            answer = i;
         }
     }
     return answer;
