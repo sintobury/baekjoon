5f52e7dc7c3d513595a2c3231e802c6c57ffe8c1
ChanUk <herkongho@gmail.com>
2023-11-10 18:05:24 +0900

    11.10문제풀이

---
+++ Programers/level2/k진수에서 소수구하기.js
@@ -0,0 +1,36 @@
+function solution(n, k) {
+    const numlist = n.toString(k).split("0").filter((el)=>el!=="");
+    console.log(n.toString(k))
+    console.log(numlist);
+    let Prime = [];
+    function isPrime(num) {
+        if(num === 1) {
+            return false;
+        }
+        if(num === 2) {
+            Prime.push(num);
+            return true;
+        }
+        if(Prime.includes(num)){
+            return true;
+        }
+
+        for(let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
+            if(num % i === 0){
+                return false; 
+            }
+        }
+        Prime.push(num);
+        return true; 
+    }
+    let answer = 0;
+    for(let i=0; i<numlist.length; i++) {
+        if(isPrime(Number(numlist[i]))) {
+            answer++;
+        }
+    }
+    console.log(answer)
+    return answer;
+}
+
+solution(	110011, 10)
