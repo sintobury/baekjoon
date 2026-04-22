05873f8cf993fbc7314cd69660de8140832272ff
허찬욱 <herkongho@gmail.com>
2026-04-22 15:43:34 +0900

    2026.4.22 문제풀이

---
+++ Programers/level1/[1차] 비밀지도.js
@@ -0,0 +1,19 @@
+function solution(n, arr1, arr2) {
+    const solved1 = arr1.map((el) => el.toString(2).padStart(n,'0'));
+    const solved2 = arr2.map((el) => el.toString(2).padStart(n,'0'));
+    let answer = new Array(n).fill(0).map(() => new Array(n).fill('#'));
+    
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+            if(solved1[i][j] === '0' && solved2[i][j] === '0'){
+                answer[i][j] = ' ';
+            } else {
+                continue;
+            }
+        }
+    }
+    answer.forEach((el,idx,arr) => arr[idx] = el.join(''));
+    return answer;
+}
+
+console.log(solution(5,[9, 20, 28, 18, 11],	[30, 1, 21, 17, 28]))
